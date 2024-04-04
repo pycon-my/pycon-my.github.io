bc2a2fd57c49d455b59ee7c8fcebe3f4f5ab891f
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-04-04 14:01:06 +0000

    Added few more files

---
+++ src/speakerLists.ts
@@ -0,0 +1,35 @@
+export const KeyNoteSpeakers = [
+    {
+      image: "https://placehold.co/400/png",
+      name: "Laura Laura",
+      title: "Data Science",
+    },
+    {
+      image: "https://placehold.co/400/png",
+      name: "Laura Laura",
+      title: "Data Science",
+    },
+  ];
+  
+export const AllSpeakers = [
+    {
+      image: "https://placehold.co/400/png",
+      name: "Laura Laura",
+      title: "Data Science",
+    },
+    {
+      image: "https://placehold.co/400/png",
+      name: "Laura Laura",
+      title: "Data Science",
+    },
+    {
+      image: "https://placehold.co/400/png",
+      name: "Laura Laura",
+      title: "Data Science",
+    },
+    {
+      image: "https://placehold.co/400/png",
+      name: "Laura Laura",
+      title: "Data Science",
+    },
+  ];
