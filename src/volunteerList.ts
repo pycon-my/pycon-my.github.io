6849a139cf33ddd0876d642c081b24ee0083830c
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-04-05 13:35:39 +0000

    Minor edit

---
+++ src/volunteerList.ts
@@ -0,0 +1,10 @@
+export const VolunteerList = [
+    {
+        name: "Abc",
+        image: "https://placehold.co/600x400/png",
+    },
+    {
+        name: "Abc",
+        image: "https://placehold.co/600x400/png",
+    },
+]
