06fd2d3237d617c616584820137ae2c515485551
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-04-05 03:46:05 +0000

    Edit Navbar

---
+++ src/previousSponsors.ts
@@ -0,0 +1,8 @@
+export const PreviousSponsorLists = [
+  {
+    img: "https://placehold.co/600x400/png",
+    alt: "Sponsor Image 1",
+    link: "https://example.com/sponsor1",
+    name: "Test",
+  },
+];
