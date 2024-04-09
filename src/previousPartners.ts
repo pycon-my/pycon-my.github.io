d12d351373dacc362de4c330b32cb55fe123e564
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-04-09 02:22:23 +0000

    Add previous partners

---
+++ src/previousPartners.ts
@@ -0,0 +1,9 @@
+export const PreviousPartnerLists = [
+    {
+      img: "https://placehold.co/600x400/png",
+      alt: "Sponsor Image 1",
+      link: "https://example.com/sponsor1",
+      name: "Test",
+    },
+  ];
+  
