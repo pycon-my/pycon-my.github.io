26f80be800b868625700eb1628aff355036cfe61
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-05-26 12:42:18 +0000

    Update keynote speaker and community partner

---
+++ src/partnerLists.ts
@@ -0,0 +1,7 @@
+export const PartnerLists = [
+    {
+      img: "/assets/sponsors/mdec-logo.png",
+      alt: "MDEC",
+      link: "https://mdec.my"
+    },
+]
--- src/speakerLists.ts
+++ src/speakerLists.ts
@@ -1,26 +1,26 @@
 export const KeyNoteSpeakers = [
     {
-      image: "https://placehold.co/400/png",
-      name: "Laura Laura",
-      title: "Data Science",
+      image: "/assets/speakers/Asaad_Abdollahzadeh.jpg",
+      name: "Asaad Abdollahzadeh",
+      title: "Senior Manager of Data Science at Petronas Digital Sdn. Bhd",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
     {
-      image: "https://placehold.co/400/png",
-      name: "Laura Laura",
-      title: "Data Science",
+      image: "/assets/speakers/Daphne_Choong.jpg",
+      name: "Daphne Choong",
+      title: "Senior Partner Development Manager of Microsoft",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
     {
-      image: "https://placehold.co/400/png",
-      name: "Laura Laura",
-      title: "Data Science",
+      image: "/assets/speakers/Iqbal_Abdullah.png",
+      name: "Iqbal Abdullah",
+      title: "CEO of LaLoka Laba",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
     {
-      image: "https://placehold.co/400/png",
-      name: "Laura Laura",
-      title: "Data Science",
+      image: "/assets/speakers/Ng_Swee_Meng.jpg",
+      name: "Ng Swee Meng",
+      title: "Senior Software Engineer with Supa",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
   ];
