293fb440cf5c9ccd3dc8787f6fb9ff4458e30390
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-05-30 01:12:29 +0000

    added new sponsors and edit countdown background

--- src/sponsorLists.ts
+++ src/sponsorLists.ts
@@ -1,82 +1,90 @@
 export const SponsorLists = [
-    {
-      header: "Platinum Sponsor",
-      sponsor: [
-        {
-          img: "https://placehold.co/800x600/png",
-          alt: "Sponsor Image 1",
-          link: "https://example.com/sponsor1",
+  {
+    header: "Platinum Sponsor",
+    sponsor: [
+      {
+        img: "/assets/sponsors/AWS_logo_RGB_BLK.png",
+        alt: "AWS",
+        link: "https://aws.amazon.com/",
+        name: "AWS",
+        desc: `
+          Amazon Web Services(AWS) introduced the first ever cloud infrastructure solution in 2006. With this came the ability to get on- demand delivery of IT resources over the internet.Without the need to invest in and maintain hardware, organizations could develop and grow significantly faster at a fraction of the cost.
+
+          Today, AWS is the most comprehensive and adopted cloud in the world.AWS powers organizations of all sizes across almost every industry in 190 countries with over 200 products, services, and custom- built solutions.From machine learning to serverless computing to internet of things, customers are using the cloud to solve ambitious business challenges and improve the way we live.Netflix is able to stream 125 million hours of entertainment to over 200 million members a day.Hawaii - based Terraformation is working towards planting a trillion trees by scaling and decentralizing seed databases globally.Capella Space is providing disaster relief organizations around the world with detailed satellite imagery in near - real time.It’s exciting, and it’s just the beginning.
+          
+          Looking ahead, we can't wait to put even more innovative cloud technology in the hands of our customers—making it faster and easier for them to build anything, solve anything, and transform the way they do business.
+          `,
         },
       ],
     },
-    {
-      header: "Gold Sponsor",
-      sponsor: [
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 1",
-          link: "https://example.com/sponsor1",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-      ],
-    },
-    {
-      header: "Silver Sponsor",
-      sponsor: [
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 1",
-          link: "https://example.com/sponsor1",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-      ],
-    },
-    {
-      header: "Bronze Sponsor",
-      sponsor: [
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 1",
-          link: "https://example.com/sponsor1",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-        {
-          img: "https://placehold.co/600x400/png",
-          alt: "Sponsor Image 2",
-          link: "https://example.com/sponsor2",
-        },
-      ],
-    },
+// {
+//   header: "Gold Sponsor",
+//     sponsor: [
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 1",
+//         link: "https://example.com/sponsor1",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//     ],
+//     },
+// {
+//   header: "Silver Sponsor",
+//     sponsor: [
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 1",
+//         link: "https://example.com/sponsor1",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//     ],
+//     },
+// {
+//   header: "Bronze Sponsor",
+//     sponsor: [
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 1",
+//         link: "https://example.com/sponsor1",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//       {
+//         img: "https://placehold.co/600x400/png",
+//         alt: "Sponsor Image 2",
+//         link: "https://example.com/sponsor2",
+//       },
+//     ],
+//     },
   ];
