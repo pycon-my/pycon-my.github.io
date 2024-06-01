a8e35d2d588cff47978dd5817eda57d6e6224c3d
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-06-01 00:55:13 +0000

    Added new keynote speaker

--- src/speakerLists.ts
+++ src/speakerLists.ts
@@ -11,7 +11,12 @@ export const KeyNoteSpeakers = [
       title: "Senior Partner Development Manager of Microsoft",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
-    
+    {
+      image: "/assets/speakers/Micaela_Reyes.jpeg",
+      name: "Micaela Reyes",
+      title: "Co-founder and Training Advisor of CodeHappy.ph",
+      desc: "Lorem ipsum"
+    }
   ];
   
 export const AllSpeakers = [
