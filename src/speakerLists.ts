6f3fa38daf9b6928e75009e5e157bac4b76feef0
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2024-06-02 14:02:11 +0000

    Update speaker card components

--- src/speakerLists.ts
+++ src/speakerLists.ts
@@ -1,18 +1,18 @@
 export const KeyNoteSpeakers = [
     {
-      image: "/assets/speakers/Asaad_Abdollahzadeh.jpg",
+      image: "/assets/speakers/Asaad_Abdollahzadeh.png",
       name: "Asaad Abdollahzadeh",
       title: "Senior Manager of Data Science at Petronas Digital Sdn. Bhd",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
     {
-      image: "/assets/speakers/Daphne_Choong.jpg",
+      image: "/assets/speakers/Daphne_Choong.png",
       name: "Daphne Choong",
       title: "Senior Partner Development Manager of Microsoft",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
     },
     {
-      image: "/assets/speakers/Micaela_Reyes.jpeg",
+      image: "/assets/speakers/Micaela_Reyes.png",
       name: "Micaela Reyes",
       title: "Co-founder and Training Advisor of CodeHappy.ph",
       desc: "Lorem ipsum"
@@ -27,7 +27,7 @@ export const AllSpeakers = [
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
   },
   {
-    image: "/assets/speakers/Ng_Swee_Meng.jpg",
+    image: "/assets/speakers/Ng_Swee_Meng.png",
     name: "Ng Swee Meng",
     title: "Senior Software Engineer with Supa",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus, ante at hendrerit mollis, massa magna ultrices diam, eu finibus ex arcu eget ligula. Integer fringilla massa eu felis porttitor, vitae tempor lorem consequat. Sed facilisis risus in purus pharetra posuere. Cras eget congue mi. Aenean efficitur quam ac libero finibus, sit amet accumsan diam pellentesque. Donec vel mauris lobortis, eleifend quam et, porta nisi. Maecenas efficitur odio elit, at consequat nisl vehicula viverra. Morbi tincidunt, dui a dictum congue, orci justo ultricies velit, nec congue velit ex nec augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id posuere nibh. Cras quis dignissim leo, eget porttitor odio. Mauris eget fermentum mi. Nullam posuere nunc non nulla dapibus porta. Donec eu bibendum sapien, et tempus diam. Suspendisse dictum iaculis metus vitae faucibus. In nec justo iaculis, dapibus urna hendrerit, malesuada turpis."
