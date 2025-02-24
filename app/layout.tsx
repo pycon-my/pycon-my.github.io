8231f193964ee5aa97b3d8d04547ddacc9c664d0
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2025-02-24 12:50:34 +0000

    📈 Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -28,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        
+        <Analytics />
       </body>
     </html>
   );
