8ac95e0ff317293240065363e440820a74049a96
Koay Hong Vin <32212455+hongvin@users.noreply.github.com>
2025-10-15 03:52:57 +0000

    Update navicat desc

--- lib/sponsors.ts
+++ lib/sponsors.ts
@@ -24,7 +24,8 @@ export const sponsors: Sponsor[] = [
     name: 'Navicat',
     logo: '/sponsor/navicat.png',
     tier: 'silver',
-    description: `Navicat is a powerful database development tool that helps you manage your databases with ease. It supports various database systems and offers a user-friendly interface for database design, querying, and administration.`
+    description: `Navicat develops leading database management and development software. Our top-rated product, Navicat Premium, allows access to up to nine databases all in one, including MySQL, PostgreSQL, MongoDB, MariaDB, SQL Server, Oracle, SQLite, Redis, and Snowflake. This integration eliminates workflow disruptions, maximizing users’ time and increasing productivity and efficiency.
+With over 25 years of experience and 5 million downloads, more than 50% of the Fortune 500 rely on Navicat every day! `
   },
   {
     name: 'Sunway University',
